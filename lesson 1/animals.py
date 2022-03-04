89d6b350fad74d440697367331a7e5d34fe54c9a
DaniyarNaamatov <mr.naamatov@mail.ru>
2022-03-04 20:05:08 +0600

    animals2

--- lesson 1/animals.py
+++ lesson 1/animals.py
@@ -1,51 +1,53 @@
 class Dog:
-    def __init__(self, weight, color, voiceText):
+    def __init__(self, name, weight, color, voiceText):
+        self.name = name
         self.weight = weight
         self.color = color
         self.voiceText = voiceText
-        """instance"""
 
     def Voice(self):
-        print(f'{self.voiceText}')
+        print(f'{self.name} {self.voiceText}')
 
-obj = Dog('weight', 'color', 'Gav Gav')
+obj = Dog('Rex:','weight', 'color', 'Gav Gav')
 obj.Voice()
 
 class Cat:
-    def __init__(self, weight, color, voiceText):
+    def __init__(self, name, weight, color, voiceText):
+        self.name = name
         self.weight = weight
         self.color = color
         self.voiceText = voiceText
-        """instance"""
 
     def Voice(self):
-        print(f'{self.voiceText}')
+        print(f'{self.name} {self.voiceText}')
 
-obj = Cat('weight', 'color', 'myau myau')
+obj = Cat('Cat:','weight', 'color', 'myau myau')
 obj.Voice()
 
 class Cow:
-    def __init__(self, weight, color, voiceText):
+    def __init__(self, name, weight, color, voiceText):
+        self.name = name
         self.weight = weight
         self.color = color
         self.voiceText = voiceText
-        """instance"""
 
     def Voice(self):
-        print(f'{self.voiceText}')
+        print(f'{self.name} {self.voiceText}')
 
-obj = Cow('weight', 'color', 'muu muu')
+obj = Cow('Cow:','weight', 'color', 'muu muu')
 obj.Voice()
 
 class Bear:
-    def __init__(self, weight, color, voiceText):
+    def __init__(self, name, weight, color, voiceText):
+        self.name = name
         self.weight = weight
         self.color = color
         self.voiceText = voiceText
-        """instance"""
+
 
     def Voice(self):
-        print(f'{self.voiceText}')
+        print(f'{self.name} {self.voiceText}')
 
-obj = Bear('weight', 'color', 'arr arr')
+obj = Bear('mixalych:','weight', 'color', 'arr arr')
 obj.Voice()
+
